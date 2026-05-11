b5f049127a4d92c6527b6207c5876617e84f630f
yunjae305 <181739058+yunjae305@users.noreply.github.com>
2026-05-11 10:21:33 +0900

    [Passed] 비교 연산 (45ms, 16MB)

---
+++ trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,37 @@
+A, B = map(int, input().split())
+
+# 1. A가 B보다 같거나 큰지
+if A >= B:
+    print(1)
+else:
+    print(0)
+
+# 2. A가 B보다 큰지
+if A > B:
+    print(1)
+else:
+    print(0)
+
+# 3. B가 A보다 같거나 큰지
+if B >= A:
+    print(1)
+else:
+    print(0)
+
+# 4. B가 A보다 큰지
+if B > A:
+    print(1)
+else:
+    print(0)
+
+# 5. A와 B가 같은지
+if A == B:
+    print(1)
+else:
+    print(0)
+
+# 6. A와 B가 다른지
+if A != B:
+    print(1)
+else:
+    print(0)
